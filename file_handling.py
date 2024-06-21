78aebf4e72a8d2a727ded3ec8928a01ba302612d
alpharay <alpharay@github.com>
2024-06-21 15:39:22 +0900

    These are my first projects

---
+++ file_handling.py
@@ -0,0 +1,9 @@
+
+#!/usr/bib/env python3
+
+
+
+myfiel = "file.txt"
+file = open(myfile, "r")
+for line in file:
+    print(line)
---
+++ handling_file.py
@@ -0,0 +1,7 @@
+
+#!/usr/bib/env python3
+
+myfile = "file.txt"
+file = open(myfile, "r")
+for line in file:
+    print(line)
